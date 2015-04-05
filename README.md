fb1566f1b20f20ed213c7ceeb7998df195a86a22
eduardo <eduardo@enrique.(none)>
2015-04-04 22:13:20 -0600

    first source

---
+++ README.md
@@ -0,0 +1,1 @@
+# onlychatroom
---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = 3000;
+ 
+app.get('/',function(req,res){
+
+res.sendFile(__dirname + '/index.html');
+});
+ 
+io.on('connection',function(socket){
+console.log("user id : %s",socket.id);
+ 
+socket.broadcast.emit('message','New User Connected ','Chat console Says');
+ 
+socket.on('message',function(msj){
+io.emit('message',msj,socket.id);
+});
+ 
+socket.on('disconnect',function(){
+console.log("Disconnect : %s",socket.id);
+});
+ 
+});
+ 
+http.listen(PORT,function(){
+console.log('The server listen to %s',PORT);
+});
