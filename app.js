d41bdf4849b2d8c274fc2817e3919b3812715f2b
Carlos Carcamo <carloscarcamo.m@gmail.com>
2015-04-05 19:39:00 -0600

    environment variable para el puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
  
 app.get('/',function(req,res){
 
